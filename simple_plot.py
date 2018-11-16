f9e84832fedd643e8a8efd99951b129a27f2a446
Misha <apple@apples-MacBook-Pro-4.local>
2018-11-16 23:00:52 +0100

    toevoegen

---
+++ simple_plot.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x=np.arange(1,10,1)
+
+
+plt.plot(x, x, label='linear')
+plt.plot(x, x**2, label='quadratic')
+plt.plot(x, x**3, label='cubic')
+
+plt.xlabel('x label')
+plt.ylabel('y label')
+
+plt.title("Simple Plot")
+
+plt.legend()
+
+plt.show()
